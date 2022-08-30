39070a03cdca2d7d518191f22a3fd7cbfd3e147f
Olya Us <olyaus0809@gmail.com>
2022-08-30 17:46:46 +0300

    Get data from marvel server

---
+++ src/services/MarvelService.js
@@ -0,0 +1,26 @@
+
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = '&apikey=6ccefb06c6909b9ab7737fa970a6afa4';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
